0797506058c3f62e428d1f096ec1bac156368055
Adi Nugroho <78519471+adinugroho07@users.noreply.github.com>
2022-01-20 14:28:46 +0700

    commit ke 3
    ini commit yang berisikan crud menggukan filesystem dimana data untuk crud kita write ke file .json

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const db = require('./util/database');
+
 const errorController = require('./controllers/error');
 
 const app = express();
+
+db.execute('select * from product')
+    .then((result) => {
+        console.log(result[0]);
+    }).catch((error) => {
+    console.error(error);
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -13,7 +22,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
